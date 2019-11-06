ded2529bf18e107a5d3c928cc5c2b149d2e3dd10
powhound4 <george.hatch@fathym.com>
2019-11-06 11:24:46 -0700

    create legend

--- projects/common/src/lib/lcu-legend.module.ts
+++ projects/common/src/lib/lcu-legend.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LegendComponent } from './controls/legend/legend.component';
+import { DeleteItemComponent } from './controls/legend/delete-item/delete-item.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LegendComponent, DeleteItemComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+  ],
+  exports: [LegendComponent, DeleteItemComponent],
+  entryComponents: [LegendComponent, DeleteItemComponent]
 })
 export class LcuLegendModule { }
